1922d2409ad00835b877b67246aad9a50763611e
vanquybn <vanquybn>
2018-01-12 16:34:38 +0700

    Update style

--- assets/js/elfinder.full.js
+++ assets/js/elfinder.full.js
@@ -1037,7 +1037,7 @@ elFinder.prototype.view = function(fm, el) {
 	 * Render one file as icon
 	*/
 	this.renderIcon = function(f) {
-		var str = '<p'+(f.tmb ? ' style="'+"background:url('"+f.tmb+"') 0 0 no-repeat"+'"' : '')+'/><label>'+this.formatName(f.name)+'</label>';
+		var str = '<p'+(f.tmb ? ' style="'+"background:url('"+f.tmb+"') no-repeat center center fixed"+';background-size:cover;"' : '')+'/><label>'+this.formatName(f.name)+'</label>';
 		if (f.link || f.mime == 'symlink-broken') {
 			str += '<em/>';
 		}
